2f0d3b2bb1bcd25a37d7794454c9528e754b8e9f
Olivia <22328406@bucks.ac.uk>
2026-02-23 21:53:55 +0000

    Adding classes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,42 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <iostream> // allows output and input
+#include <vector> // for use of book vectors
+#include <string> // for string attributes
 
-using namespace std;
+using namespace std; // efficiency, removes typing in std:: each time
 
-int main() {
+// creating the book class
+class Book {
+
+};
+
+// creating the user class
+class User {
+
+};
+
+// creating the library member class
+class LibraryMember {
+
+};
+
+// creating the librarian class
+class Librarian {
+
+};
+
+// creating the administrator class
+class Administrator {
+
+};
+
+// creating the library system class
+class LibrarySystem {
+
+};
+
+
+int main() { // start of program
+
     return 0;
+
 }
